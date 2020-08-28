918b82a3c7cfc1c81b3f39c61175f0811be9aeb0
苏犇 <suben@formssi.com>
2020-08-28 15:52:38 +0800

    initial commit

---
+++ src/main/java/com/itsu/itsutoken/ItsuTokenApplication.java
@@ -0,0 +1,13 @@
+package com.itsu.itsutoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsuTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItsuTokenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itsu/itsutoken/ItsuTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsu.itsutoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItsuTokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
