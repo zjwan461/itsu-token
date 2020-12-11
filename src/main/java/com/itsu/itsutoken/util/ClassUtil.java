8876f9b539d971b1318e1760d7e5b5a7fc7da4fc
suben007 <suben@formssi.com>
2020-12-11 14:00:17 +0800

    自定义token校验添加一些工具方法

--- src/main/java/com/itsu/itsutoken/util/ClassUtil.java
+++ src/main/java/com/itsu/itsutoken/util/ClassUtil.java
@@ -2,6 +2,7 @@ package com.itsu.itsutoken.util;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,6 +10,7 @@ import com.itsu.itsutoken.annotation.PrivateKey;
 import com.itsu.itsutoken.annotation.PublicKey;
 import com.itsu.itsutoken.annotation.SimpleToken;
 import com.itsu.itsutoken.annotation.SysName;
+import com.itsu.itsutoken.annotation.TableField;
 import com.itsu.itsutoken.annotation.TableId;
 import com.itsu.itsutoken.domain.IdType;
 import com.itsu.itsutoken.table.TableSample;
@@ -20,47 +22,64 @@ import cn.hutool.core.annotation.AnnotationUtil;
 
 public class ClassUtil {
 
-    public static String getId(@NonNull Class<? extends TableSample> clazz) throws Exception {
-        return getValue(clazz, TableId.class);
-    }
+	public static String getId(@NonNull Class<? extends TableSample> clazz) throws Exception {
+		return getValue(clazz, TableId.class);
+	}
 
-    public static IdType getIdStrategy(@NonNull Class<? extends TableSample> clazz) throws Exception {
-        IdType idType = null;
-        List<Field> fields = Arrays.asList(clazz.getDeclaredFields());
-        for (Field field : fields) {
-            if (field.isAnnotationPresent(TableId.class)) {
-                idType = AnnotationUtil.getAnnotationValue(field, TableId.class, "type");
-                break;
-            }
-        }
-        return idType;
-    }
+	public static IdType getIdStrategy(@NonNull Class<? extends TableSample> clazz) throws Exception {
+		IdType idType = null;
+		List<Field> fields = Arrays.asList(clazz.getDeclaredFields());
+		for (Field field : fields) {
+			if (field.isAnnotationPresent(TableId.class)) {
+				idType = AnnotationUtil.getAnnotationValue(field, TableId.class, "type");
+				break;
+			}
+		}
+		return idType;
+	}
 
-    public static String getSysValue(@NonNull Class<? extends TableSample> clazz) throws Exception {
-        return getValue(clazz, SysName.class);
-    }
+	public static String getSysValue(@NonNull Class<? extends TableSample> clazz) throws Exception {
+		return getValue(clazz, SysName.class);
+	}
 
-    public static String getSimpleTokenValue(@NonNull Class<? extends TableSample> clazz) throws Exception {
-        return getValue(clazz, SimpleToken.class);
-    }
+	public static String getSimpleTokenValue(@NonNull Class<? extends TableSample> clazz) throws Exception {
+		return getValue(clazz, SimpleToken.class);
+	}
 
-    public static String getPrivateKeyValue(@NonNull Class<? extends TableSample> clazz) throws Exception {
-        return getValue(clazz, PrivateKey.class);
-    }
+	public static String getPrivateKeyValue(@NonNull Class<? extends TableSample> clazz) throws Exception {
+		return getValue(clazz, PrivateKey.class);
+	}
 
-    public static String getPublicKeyValue(@NonNull Class<? extends TableSample> clazz) throws Exception {
-        return getValue(clazz, PublicKey.class);
-    }
+	public static String getPublicKeyValue(@NonNull Class<? extends TableSample> clazz) throws Exception {
+		return getValue(clazz, PublicKey.class);
+	}
 
-    private static String getValue(Class<? extends TableSample> clazz, Class<? extends Annotation> annotationType) {
-        String value = null;
-        List<Field> fields = Arrays.asList(clazz.getDeclaredFields());
-        for (Field field : fields) {
-            if (!StringUtils.isEmpty(AnnotationUtil.getAnnotationValue(field, annotationType))) {
-                value = AnnotationUtil.getAnnotationValue(field, annotationType);
-                break;
-            }
-        }
-        return value;
-    }
+	public static String getTableFieldValue(@NonNull Class<? extends TableSample> clazz) throws Exception {
+		return getValue(clazz, TableField.class);
+	}
+
+	public static List<String> getTableFieldValues(@NonNull Class<? extends TableSample> clazz) throws Exception {
+		return getValues(clazz, TableField.class);
+	}
+
+	private static String getValue(Class<? extends TableSample> clazz, Class<? extends Annotation> annotationType) {
+		List<String> values = getValues(clazz, annotationType);
+		if (values.size() > 0)
+			return values.get(0);
+		else
+			return "";
+	}
+
+	private static List<String> getValues(Class<? extends TableSample> clazz,
+			Class<? extends Annotation> annotationType) {
+		List<String> list = new ArrayList<>();
+		List<Field> fields = Arrays.asList(clazz.getDeclaredFields());
+		for (Field field : fields) {
+			if (!StringUtils.isEmpty(AnnotationUtil.getAnnotationValue(field, annotationType))) {
+				String value = AnnotationUtil.getAnnotationValue(field, annotationType);
+				list.add(value);
+			}
+		}
+		return list;
+	}
 }
